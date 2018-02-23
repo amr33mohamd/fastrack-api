175bc050db55924f91fe73d847cb2a61ab6a22b4
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-02-23 15:54:45 +0200

    notes

--- routes/api/subjects.js
+++ routes/api/subjects.js
@@ -1,6 +1,6 @@
 app.get('/api/subjects',function(req,res){
   var university_id = req.query.id;
-    con.query('SELECT id, name,description FROM subjects where university_id= ? ORDER BY id ASC ',[university_id], function(err,data) {
+    con.query('SELECT id, name FROM subjects where university_id= ? ORDER BY id ASC ',[university_id], function(err,data) {
         if(!err) {
             res.send(data);
         }
