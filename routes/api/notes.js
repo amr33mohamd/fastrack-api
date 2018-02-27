363ce2330b9283157f0613e26b82314f9bc581db
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-02-27 21:10:48 +0200

    lll

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -9,3 +9,15 @@ app.get('/api/notes',function(req,res){
         }
     })
 });
+
+app.get('/api/searchnotes',function(req,res){
+  var subject_id = req.query.id;
+    con.query("SELECT id, name,descc AS `description`,image,price FROM notes where name  LIKE '%?%' ORDER BY id ASC ",[subject_id], function(err,data) {
+        if(!err) {
+            res.send(data);
+        }
+        else{
+          res.send(err);
+        }
+    })
+});
