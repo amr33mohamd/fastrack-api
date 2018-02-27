c5b1f1c2855b2e4f5d9f037e53583816c224da22
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-02-27 05:12:32 +0200

    aa

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,6 +1,6 @@
 app.get('/api/notes',function(req,res){
   var subject_id = req.query.id;
-    con.query('SELECT id, name,descc,image,price FROM notes where subject_id= ? ORDER BY id ASC ',[subject_id], function(err,data) {
+    con.query('SELECT id, name,descc AS `description`,image,price FROM notes where subject_id= ? ORDER BY id ASC ',[subject_id], function(err,data) {
         if(!err) {
             res.send(data);
         }
