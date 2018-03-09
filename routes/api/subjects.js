b1301302442b1ba47a665fb7171e68a76d0b2461
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-03-09 21:17:18 +0200

    aaa

--- routes/api/subjects.js
+++ routes/api/subjects.js
@@ -1,6 +1,19 @@
 app.get('/api/subjects',function(req,res){
   var university_id = req.query.id;
-    con.query('SELECT id, name FROM subjects where university_id= ? ORDER BY id ASC ',[university_id], function(err,data) {
+    con.query('SELECT id, name FROM subjects where sector_id= ? ORDER BY id ASC ',[university_id], function(err,data) {
+        if(!err) {
+            res.send(data);
+        }
+        else{
+          res.send(err);
+        }
+    })
+});
+
+
+app.get('/api/sectors',function(req,res){
+  var university_id = req.query.id;
+    con.query('SELECT id, name FROM sectors where university_id= ? ORDER BY id ASC ',[university_id], function(err,data) {
         if(!err) {
             res.send(data);
         }
