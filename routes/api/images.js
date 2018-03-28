2172898a963d39575df950e9e2c466474e17494f
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-03-28 19:14:24 +0200

    freenotes

--- routes/api/images.js
+++ routes/api/images.js
@@ -2,7 +2,13 @@ app.get('/api/images',function(req,res){
   var note_id = req.query.id;
     con.query('SELECT id, image FROM images where note_id= ? ORDER BY id ASC ',[note_id], function(err,data) {
         if(!err) {
-            res.send(data);
+          var images = [];
+          for(let i in data){
+            images.push({ source: { uri: data[i].image } });
+            if(i == data.length-1){
+              res.send(images);
+            }
+          }
         }
         else{
           res.send(err);
