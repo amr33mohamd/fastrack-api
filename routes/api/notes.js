ba2f1defc91dcc5e9d558b330662b6a6919c8b95
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-03-28 20:26:05 +0200

    aaa

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -10,6 +10,23 @@ app.get('/api/notes',function(req,res){
     })
 });
 
+app.get('/api/mynotes',function(req,res){
+  var id = req.query.id;
+  var mynotes = [];
+  con.query('SELECT note_id FROM 	ownedNotes where deviceId= ?',[id],function(err,notes){
+    for(let i in notes){
+      con.query('SELECT id, name,descc AS `description`,image,price FROM notes where id= ? ',[notes[i].note_id], function(errr,data) {
+        mynotes.push(data[0]);
+        if(i == notes.length-1){
+          res.send(mynotes);
+        }
+      })
+
+    }
+  })
+
+});
+
 app.get('/api/freenotes',function(req,res){
   var subject_id = req.query.id;
     con.query('SELECT id, name,descc AS `description`,image,price FROM notes where subject_id= ? and price= ? ORDER BY name ',[subject_id,0], function(err,data) {
