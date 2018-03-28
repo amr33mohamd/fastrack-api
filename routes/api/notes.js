9002a24bc7726f00911624753e05668b187834d0
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-03-28 17:27:44 +0200

    freenotes

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -10,6 +10,18 @@ app.get('/api/notes',function(req,res){
     })
 });
 
+app.get('/api/freenotes',function(req,res){
+  var subject_id = req.query.id;
+    con.query('SELECT id, name,descc AS `description`,image,price FROM notes where subject_id= ? and price= ? ORDER BY name ',[subject_id,0], function(err,data) {
+        if(!err) {
+            res.send(data);
+        }
+        else{
+          res.send(err);
+        }
+    })
+});
+
 app.get('/api/searchnotes',function(req,res){
   var subject_id = req.query.id;
     con.query("SELECT id, name,descc AS `description`,image,price FROM notes where name  LIKE ? ORDER BY name ",['%'+subject_id+'%'], function(err,data) {
