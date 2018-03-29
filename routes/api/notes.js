3a83933dda79033c7f71889bf094b91163ffbf93
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-03-29 03:00:39 +0200

    aaa

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -27,6 +27,27 @@ app.get('/api/mynotes',function(req,res){
 
 });
 
+app.get('/try',function(req,res){
+  var pdftohtml = require('pdftohtmljs');
+var converter = new pdftohtml('books/0.60416769657951571.pdf', "sample.html");
+
+// See presets (ipad, default)
+// Feel free to create custom presets
+// see https://github.com/fagbokforlaget/pdftohtmljs/blob/master/lib/presets/ipad.js
+// convert() returns promise
+converter.convert('ipad').then(function() {
+  console.log("Success");
+}).catch(function(err) {
+  console.error("Conversion error: " + err);
+});
+
+// If you would like to tap into progress then create
+// progress handler
+converter.progress(function(ret) {
+  console.log ((ret.current*100.0)/ret.total + " %");
+});
+})
+
 app.get('/api/freenotes',function(req,res){
   var subject_id = req.query.id;
     con.query('SELECT id, name,descc AS `description`,image,price FROM notes where subject_id= ? and price= ? ORDER BY name ',[subject_id,0], function(err,data) {
--- routes/web.js
+++ routes/web.js
@@ -403,7 +403,27 @@ app.get('/add-university', function(req, res) {
 });
 
 
+app.get('/pdf2image',function(req,res){
+	var fs      = require('fs');
+var path    = require('path');
+var pdf2img = require('pdf2img');
 
+var input   = __dirname + '/books/0.60416769657951571.pdf';
+console.log(input);
+pdf2img.setOptions({
+  type: 'png',                                // png or jpg, default jpg
+  size: 1024,                                 // default 1024
+  density: 600,                               // default 600
+  outputdir: null, // output folder, default null (if null given, then it will create folder name same as file name)
+  outputname:null,                         // output file name, dafault null (if null given, then it will create image name same as input name)
+  page: null                                  // convert selected page, default null (if null given, then it will convert all pages)
+});
+
+pdf2img.convert(input, function(err, info) {
+  if (err) console.log(err)
+  else console.log(info);
+});
+})
 
 app.post('/add_university', function(req, res) {
 	var image = req.files.image;
