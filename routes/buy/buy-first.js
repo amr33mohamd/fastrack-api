82400da130a6d6a03e39b8944849c68ac0a1a5b4
amr33mohamd <amr2010mohamd2010@gmail.com>
2019-02-05 01:21:42 +0200

    AA

--- routes/buy/buy-first.js
+++ routes/buy/buy-first.js
@@ -47,7 +47,7 @@ app.get('/buy-first',function(req,res){
              "SendInvoiceOption": 1,
              "InvoiceItemsCreate": [
              {
-             "ProductId": note[0].id,
+             "ProductId": null,
              "ProductName": "Product01",
              "Quantity": 1,
              "UnitPrice": note[0].price
