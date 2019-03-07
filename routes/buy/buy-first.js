1be3858740d4d5525adb4a2f408afe49ee057927
amr33mohamd <amr2010mohamd2010@gmail.com>
2019-03-07 18:50:48 +0200

    short url

--- routes/buy/buy-first.js
+++ routes/buy/buy-first.js
@@ -29,7 +29,7 @@ app.get('/buy-first',function(req,res){
 
           console.log("r"+redirect_url)
 
-          shortUrl.short(redirect_url, function(err, test){
+          shortUrl.short('http://' + req.headers.host + '/buy-second?note_id=' + note[0].id + '&deviceId=' + deviceId + '&type=1', function(err, test){
 console.log("llll"+test)
               // var url = 'https://php-helper.herokuapp.com/try.php?price='+note[0].price+'&url=http://'+req.headers.host+'/buy-second?note_id='+note[0].id+','+deviceId+',1';
           const options = {
